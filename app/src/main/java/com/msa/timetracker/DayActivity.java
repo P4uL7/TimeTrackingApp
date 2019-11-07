6f42d5fe2fd52f0bf8aaff9a3f91f72ea6f06917
stanila <stanila_paul@yahoo.com>
2019-11-07 14:04:00 +0200

    Nav drawer update 2.

--- app/src/main/java/com/msa/timetracker/DayActivity.java
+++ app/src/main/java/com/msa/timetracker/DayActivity.java
@@ -1,11 +1,17 @@
 package com.msa.timetracker;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class DayActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +21,27 @@ public class DayActivity extends AppCompatActivity {
 //        Button mBtGoBack = findViewById(R.id.bt_go_back);
 //        mBtGoBack.setOnClickListener(view -> finish());
 
+        Button but = findViewById(R.id.bt_go_back);
+        but.setOnClickListener(view -> Toast.makeText(DayActivity.this, "click",
+                Toast.LENGTH_SHORT).show());
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
